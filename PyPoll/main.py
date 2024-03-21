09502e648d8fc364d102efa071ab03b04fca992d
Kopano Majara <kops4jc@gmail.com>
2024-03-21 16:22:40 -0400

    Updating Github after completion of Pypoll

---
+++ PyPoll/main.py
@@ -0,0 +1,65 @@
+import os
+import csv
+
+# provide file path
+file_path = os.path.join('PyPoll','Resources','election_data.csv')
+output_path = os.path.join('PyPoll', 'analysis', 'election_analysis.txt')
+
+#variables
+total_votes = 0
+candidate_1 = 'Charles Casper Stockham'
+charles_casper_votes = 0
+candidate_2 = 'Diana DeGette'
+diana_degette_votes = 0
+candidate_3 = 'Raymon Anthony Doane'
+raymon_anthony_votes = 0
+
+
+with open(file_path, 'r') as csvfile:
+  csv_reader = csv.reader(csvfile, delimiter=",")
+
+  #Read the header row
+  csv_header = next(csv_reader)
+
+  #Iterate through each row in the file starting after the header
+  for row in csv_reader: 
+    #calculate total votes
+    total_votes +=1
+    
+    #list of candidates who won
+    if row[2] == candidate_1:
+        charles_casper_votes +=1
+
+    elif row[2] == candidate_2:
+        diana_degette_votes +=1
+
+    elif row[2] == candidate_3:
+        raymon_anthony_votes +=1
+    
+     #calculate percentages of votes won 
+  css_percentage = round(charles_casper_votes/total_votes * 100, 3)
+  ddg_percentage = round(diana_degette_votes/total_votes * 100, 3)
+  rad_percentage = round(raymon_anthony_votes/total_votes * 100, 3)
+
+#determine winner
+  votes = [charles_casper_votes, diana_degette_votes, raymon_anthony_votes]
+  candidates = [candidate_1, candidate_2, candidate_3]
+  winner = candidates[votes.index(max(votes))]
+
+   
+output = ("\nElection Results\n"
+          f"\n------------------------------'\n\n"
+  f"Total Votes: {total_votes}\n"
+  f"\n------------------------------'\n"
+  f"Charles Casper Stockham: {css_percentage}% ({charles_casper_votes})\n"
+  f"Diana DeGette: {ddg_percentage}% ({diana_degette_votes})\n"
+  f"Raymon Anthony Doane: {rad_percentage}% ({raymon_anthony_votes})\n"
+  f"\n------------------------------\n"
+  f"\nWinner: {winner}\n"
+  f"\n------------------------------\n")
+
+print(output)
+
+with open(output_path, "w") as txtfile: 
+   txtfile.write(output)
+ 
